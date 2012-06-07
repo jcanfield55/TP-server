d34d0ff2e8563303eb6be065ddb9896d6c5a6b3d
jkumbhani <jkumbhani@gmail.com>
2012-06-07 18:18:55 +0530

    Adding monitoring, logging, feedback, features on TPServer

--- projectTP/test/mongo/TestMongoApp.java
+++ projectTP/test/mongo/TestMongoApp.java
@@ -1,14 +1,11 @@
 package mongo;
 import java.util.ArrayList;
-import java.util.List;
 
-import com.apprika.otp.TPApplicationContext;
-import com.apprika.otp.TPApplicationContext.SPRING_BEANS;
-import com.apprika.otp.dbobject.FeedBack;
-import com.apprika.otp.dbobject.User;
-import com.apprika.otp.mongo.PersistanceService;
-import com.apprika.otp.startup.StartupManager;
-import com.apprika.otp.util.TpConstants.MONGO_TABLES;
+import com.nimbler.tp.TPApplicationContext;
+import com.nimbler.tp.TPApplicationContext.SPRING_BEANS;
+import com.nimbler.tp.dbobject.EventLog;
+import com.nimbler.tp.mongo.PersistanceService;
+import com.nimbler.tp.startup.StartupManager;
 
 public class TestMongoApp {
 
@@ -23,19 +20,27 @@ public class TestMongoApp {
 		//		Employee employee3 = new Employee("Nikil", "Patel", 23,null, company2);
 
 		//		
-		User user = new User();
-		user.setDeviceId("device");
+		//		User user = new User();
+		//		user.setDeviceId("device");
 		//		user.setCreateTime(System.currentTimeMillis());
 
-		//		service.addObject(MONGO_TABLES.users.name(), user);
-		List<FeedBack> lst = service.getCollectionList(MONGO_TABLES.feedback.name(),FeedBack.class);
 
+		//		GeoCode code = new GeoCode("rawAddrassFrom0", "rawAddrassTo0", "formattedAddrassFrom0", null);
+		EventLog log = new EventLog();
+		//Map map = new HashMap();
+		//map.put("hello", "hello1");
+		//map.put("hi", "hi1");
+		//		log.setData(code);
+		//		log.setType(EVENT_TYPE.GEO_CODE_FROM.ordinal());
 
-		System.out.println(user.getId());
-		//		List<User> lst = service.getCollectionList(MONGO_TABLES.users.name(), User.class);
-		for (FeedBack user2 : lst) {
-			System.out.println(user2);
-		}
+		//		service.addObject(MONGO_TABLES.event_log.name(), log);
+		//List<FeedBack> lst = service.getCollectionList(MONGO_TABLES.event_log.name(),AuditLog.class);
+		System.out.println(log.getId());
+
+		//		List<EventLog> lst1 = service.getCollectionList(MONGO_TABLES.event_log.name(), EventLog.class);		
+		//		for (EventLog user2 : lst1) {
+		//			System.out.println(user2);
+		//		}
 
 	}
 }
